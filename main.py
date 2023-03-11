365dab45ae94ffecef530692e9996f38c8d9bacd
Isaac Manzi-Rickaby <imanzirickaby@gmail.com>
2023-03-11 03:15:41 +0000

    Remove external dependencies
    -SKIMAGE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from skimage import io
+import imageio
 import numpy as np
 
 if __name__ == '__main__':
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     b_channel_filename = os.path.join(os.getcwd(), sys.argv[3])
     splat_map_filename = os.path.join(os.getcwd(), sys.argv[4])
 
-    # Open the three input PNG files
-    r_channel = io.imread(r_channel_filename)
-    g_channel = io.imread(g_channel_filename)
-    b_channel = io.imread(b_channel_filename)
+    # Open the three input PNG files with imageio
+    r_channel = imageio.imread(r_channel_filename)
+    g_channel = imageio.imread(g_channel_filename)
+    b_channel = imageio.imread(b_channel_filename)
 
     # Merge the three channels into a single RGB image
     splat_map = np.dstack((r_channel, g_channel, b_channel))
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     # Convert the output image to uint16 to preserve full precision
     splat_map = splat_map.astype(np.uint16)
 
-    # Save the output image as a PNG file
-    io.imsave(splat_map_filename, splat_map)
+    # Save the output image as a PNG file with imageio
+    imageio.imwrite(splat_map_filename, splat_map)
